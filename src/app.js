1827dc7f04026a1a65bf0ccc5e43aef552c58950
mariacaseiro <mariacaria@inditex.com>
2023-11-16 11:16:07 +0100

    Functional version with images

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import {
   configure,
   hits,
   pagination,
-  panel,
   refinementList,
   searchBox,
+  rangeSlider
 } from 'instantsearch.js/es/widgets';
 
 const searchClient = algoliasearch(
@@ -31,7 +31,7 @@ search.addWidgets([
     templates: {
       item: (hit, { html, components }) => html`
         <div>
-          <img src=${hit.url_photo} align="center"/>
+          <img src="${hit.url_foto}" align="center" width="150px" height="250px"/>
           <div class="hit-name">
             ${components.Highlight({ hit, attribute: 'nombre' })}
           </div>
@@ -40,15 +40,24 @@ search.addWidgets([
           </div>
           <div class="hit-size">Tallas: ${components.Highlight({ hit, attribute: 'tallas' })}</div>
           <div class="hit-price">${components.Highlight({ hit, attribute: 'precio' })}</div>
+          <p></p>
+          <form action="${hit.url_producto}">
+            <input class="btn" type="submit" value="VER PRODUCTO" />
+          </form>
         </div>
       `,
     },
   }),
   configure({
-    hitsPerPage: 9,
+    hitsPerPage: 12,
+  }),
+  refinementList({
+    container: '#range-slider',
+    attribute: 'tallas',
   }),
   pagination({
     container: '#pagination',
+    scrollTo: 'header'
   }),
 ]);
 
